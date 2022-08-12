ffe6aa39fc9640066c66b81c118959fde24a63ba
Francis <fwang36@gmail.com>
2022-08-12 00:51:43 -0400

    create categoryItem

---
+++ components/CategoryItem/index.jsx
@@ -0,0 +1,20 @@
+import {View, Text, StyleSheet} from 'react-native'
+import MenuItem from '../MenuItem'
+export default function CategoryItem(props) {
+
+  return (
+    <View>
+      <Text>
+        Category Test
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  categoryContainer: {
+    fontSize: 32,
+
+  }
+
+})
