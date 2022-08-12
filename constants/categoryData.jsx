ca902278b35d6edcb913f2d255b756b24b56c47c
Francis <fwang36@gmail.com>
2022-08-12 01:27:13 -0400

    add category data

---
+++ constants/categoryData.jsx
@@ -0,0 +1,10 @@
+export const categoryData = [
+  'Handmade Specialities 面食',
+  'Noodle Bowls 面条',
+  'Rice Dishes 盒饭',
+  'Main Dishes 特色小炒',
+  'Soups 汤',
+  'Congee 粥',
+  'Sides 小菜',
+  'Drinks 饮品'
+]
