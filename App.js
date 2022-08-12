df8c85fa9cbf4109d64f866c9dec57ad2dbab9da
Francis <fwang36@gmail.com>
2022-08-12 00:11:40 -0400

    inital commit

--- App.js
+++ App.js
@@ -3,9 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={styles.title}>
+        Wei Xiang Yuan Menu
+      </Text>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -17,4 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    // fontSize: 'xx-large',
+    fontWeight: '700',
+    textAlign: 'center'
+  }
 });
