b286c7131f9228069f02c25eabe181bd796e2d2d
Francis <fwang36@gmail.com>
2022-08-12 12:20:04 -0400

    change main container to scroll view

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import CategoryItem from './components/CategoryItem';
 import MenuItem from './components/MenuItem';
-// import { categoryData } from '../constants/categoryData';
-
+import { categoryData } from './constants/categoryData'
 export default function App() {
-
-  const categoryData = [
-    'Handmade Specialities 面食',
-    'Noodle Bowls 面条',
-    'Rice Dishes 盒饭',
-    'Main Dishes 特色小炒',
-    'Soups 汤',
-    'Congee 粥',
-    'Sides 小菜',
-    'Drinks 饮品'
-  ]
 
 const mappedCategories = categoryData.map(category => {
   return (
@@ -30,10 +18,10 @@ const mappedCategories = categoryData.map(category => {
       <Text style={styles.title}>
         Wei Xiang Yuan Menu
       </Text>
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         {mappedCategories}
         <StatusBar style="auto" />
-      </View>
+      </ScrollView>
     </View>
   );
 }
